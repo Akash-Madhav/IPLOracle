62c1d9466ea3f0e8defaf0234773c516b724d599
Akash-Madhav <akashmadhav@gmail.com>
2025-11-14 23:31:32 +0530

    jhbb

--- backend/services/embedding.py
+++ backend/services/embedding.py
@@ -1,24 +1,20 @@
-# services/embedding.py
-
-from sentence_transformers import SentenceTransformer
+from functools import lru_cache
 import gc
-model = None
 
+@lru_cache()
 def get_model():
-    global model
-    if model is None:
-        print("🔥 Loading MiniLM model (lazy)...")
-        model = SentenceTransformer("paraphrase-MiniLm-L3-v2", device="cpu")
-        print("✅ MiniLM loaded")
+    from sentence_transformers import SentenceTransformer
+    print("🔥 Loading MiniLM model (lazy)...")
+    model = SentenceTransformer("paraphrase-MiniLm-L3-v2", device="cpu")
+    print("✅ MiniLM loaded")
     return model
 
 def get_embedding(text: str):
     model = get_model()
     return model.encode(text).tolist()
+
 def clear_model():
-    global model
-    if model is not None:
-        del model
-        model = None
-        gc.collect()
-        print("🗑️ MiniLM model cleared from memory")
+    import torch
+    torch.cuda.empty_cache()  # if using GPU
+    gc.collect()
+    print("🗑️ MiniLM model cleared from memory")
